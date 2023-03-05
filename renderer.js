3cbf620e42a9c2fa8277b6770e418bdecf4f6dbb
oyzr1234 <ouyangzongrun@qq.com>
2023-03-05 19:04:25 +0800

    更新默认1组长名单

--- renderer.js
+++ renderer.js
@@ -14,7 +14,7 @@ document.getElementById('fill3').addEventListener('click', () => {
 	document.getElementById('fs').value = "23 25 33 30 39 5 4 29 11 26 32 2 43 9";
 })
 document.getElementById('fill4').addEventListener('click', () => {
-	document.getElementById('zz').value = "3 17 19 29 31 37 38";
+	document.getElementById('zz').value = "2 4 10 16 19 20 23 24 25 26 27 28 29 30 31 32 33 34 35 37 38 39 41 42 43 44";
 })
 
 const logBox = document.getElementsByClassName('log-box')[0];
